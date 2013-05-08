9affd3e2aad9835e8a0cb1b847c408b14ca035dc
happyaccidents <hello@happyaccidents.me>
2013-05-08 20:48:44 +0100

    Routes and shit

--- modules/App/config/routes.php
+++ modules/App/config/routes.php
@@ -3,76 +3,41 @@
 return [
     /**
      * -------------------------------------------------------------------------
-     * Routes that should respond to any HTTP request verb
+     * Routes
+     * -------------------------------------------------------------------------
+     * Define routes and their destinations, either point a route at a string of
+     * ClassName::actionName, a closure or an array with multiple destinations
+     * for different request methods.
+     * -------------------------------------------------------------------------
+     * Example
+     * -------------------------------------------------------------------------
+     * 1. String
+     * -------------------------------------------------------------------------
+     * '/route' => 'Namespace\ControllerName::action'
+     * -------------------------------------------------------------------------
+     * 2. Closure
+     * -------------------------------------------------------------------------
+     * '/route2' => function () { return 'Hello World!'; }
+     * -------------------------------------------------------------------------
+     * 3. Multiple Request Methods
+     * -------------------------------------------------------------------------
+     * '/route' => [
+     *     'get' => 'Namespace\ControllerName::actionOne',
+     *     'post' => 'Namespace\ControllerName::actionTwo'
+     * ]
      * -------------------------------------------------------------------------
      */
-    'any' => [
-        '/' => 'App\Controller\Home::index',
+    '/' => 'App\Controller\Home::index',
 
-        // Custom 404 route
-        '/404' => function () {
-            $view = $this->getContainer()->resolve('Orno\Mvc\View\Renderer');
-            $view->region('content', '<h1>Error 404</h1><p>Page Not Found</p>');
-            return $view->render();
-        }
-    ],
+
     /**
      * -------------------------------------------------------------------------
-     * Routes that should respond to the GET HTTP request verb
+     * Custom 404 Route
      * -------------------------------------------------------------------------
      */
-    'get' => [
-
-    ],
-    /**
-     * -------------------------------------------------------------------------
-     * Routes that should respond to the POST HTTP request verb
-     * -------------------------------------------------------------------------
-     */
-    'post' => [
-
-    ],
-    /**
-     * -------------------------------------------------------------------------
-     * Routes that should respond to the PUT HTTP request verb
-     * -------------------------------------------------------------------------
-     */
-    'put' => [
-
-    ],
-    /**
-     * -------------------------------------------------------------------------
-     * Routes that should respond to the PATCH HTTP request verb
-     * -------------------------------------------------------------------------
-     */
-    'patch' => [
-
-    ],
-    /**
-     * -------------------------------------------------------------------------
-     * Routes that should respond to the DELETE HTTP request verb
-     * -------------------------------------------------------------------------
-     */
-    'delete' => [
-
-    ],
-    /**
-     * -------------------------------------------------------------------------
-     * Routes that should respond to the OPTIONS HTTP request verb
-     * -------------------------------------------------------------------------
-     */
-    'options' => [
-
-    ],
-    /**
-     * -------------------------------------------------------------------------
-     * Restful Routes
-     * -------------------------------------------------------------------------
-     * Defining a restful route will create routes for all of the request verbs
-     * above. See http://getorno.com/framework/http-verbs
-     * -------------------------------------------------------------------------
-     */
-    'restful' => [
-
-    ]
+    '/404' => function () {
+        $view = $this->getContainer()->resolve('Orno\Mvc\View\Renderer');
+        $view->region('content', '<h1>Error 404</h1><p>Page Not Found</p>');
+        return $view->render();
+    }
 ];
